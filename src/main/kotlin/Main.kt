fd1d7ac292162de44b274c227ab8ae9eb01f153e
oladejomubarak <oladejomubarakade@gmail.com>
2023-03-15 10:46:10 +0000

    data types

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+    val minIntegerValue: Int = Int.MIN_VALUE
+    val maxIntegerValue: Int = Int.MAX_VALUE
+    println("min is: $minIntegerValue")
+    println("max is: $maxIntegerValue")
+}
