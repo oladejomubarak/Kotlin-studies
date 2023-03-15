a2e686dcbdcea3bec97af8dc6c46f8facef02737
oladejomubarak <oladejomubarakade@gmail.com>
2023-03-15 17:30:28 +0000

    control flow

--- src/main/kotlin/ControlFlow.kt
+++ src/main/kotlin/ControlFlow.kt
@@ -1,11 +1,8 @@
 fun main(){
-    val time = 5
+    val time = 3
 
     when(time){
-        5 -> println("The time is $time")
-        6 -> println("The time is $time")
-        3 -> println("The time is $time")
-        4 -> println("The time is $time")
+        5, 6, 3, 4-> println("The time is $time")
         else -> println("The time is $time")
     }
 }
