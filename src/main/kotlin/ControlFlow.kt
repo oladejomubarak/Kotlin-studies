c2470821f5f54afc39cb3bd9815ccdd5227dfa3d
oladejomubarak <oladejomubarakade@gmail.com>
2023-03-15 17:24:48 +0000

    control flow

---
+++ src/main/kotlin/ControlFlow.kt
@@ -0,0 +1,11 @@
+fun main(){
+    val time = 5
+
+    when(time){
+        5 -> println("The time is $time")
+        6 -> println("The time is $time")
+        3 -> println("The time is $time")
+        4 -> println("The time is $time")
+        else -> println("The time is $time")
+    }
+}
