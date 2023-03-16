0488af207466d124687f66058eab2e4398ae2553
oladejomubarak <oladejomubarakade@gmail.com>
2023-03-16 16:05:41 +0000

    functions

--- src/main/kotlin/Funtions.kt
+++ src/main/kotlin/Funtions.kt
@@ -1,7 +1,12 @@
 fun main (){
-    println(getMax(10, 5))
+    //println(getMax(10, 5))
+    sendMessage(message = "Hello there", name =  "Mubarak")
 }
 fun getMax(a:Int, b:Int):Int{
     val max = if (b < a) a else b
     return max
+}
+
+fun sendMessage(name :String = "", message:String = "Hello!"){
+    println("Name is ${name} and message is ${message}")
 }
