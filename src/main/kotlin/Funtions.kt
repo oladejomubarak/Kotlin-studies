ba12246bf32d11ef9ec5799c772267958c7c5cde
oladejomubarak <oladejomubarakade@gmail.com>
2023-03-16 17:19:41 +0000

    default parameter for functions

--- src/main/kotlin/Funtions.kt
+++ src/main/kotlin/Funtions.kt
@@ -3,6 +3,9 @@ fun main (){
     sendMessage(message = "Hello there", name =  "Oladejo")
     sendMessage(message = "How far")
     sendMessage()
+
+    println(print(1,2,3,34,45,6))
+    println(sum(1,2,3,34,45,6))
 }
 fun getMax(a:Int, b:Int):Int{
     val max = if (b < a) a else b
@@ -13,3 +16,14 @@ fun sendMessage(name :String = "Mubarak", message:String = setText()){
     println("Name is ${name} and message is ${message}")
 }
 fun setText() = "some text"
+
+fun sum(vararg numbers:Int):Int{
+    var result = 0
+
+    for (number in numbers){
+        result += number
+    }
+    return result
+}
+
+fun print(vararg numbers: Int) = numbers.forEach { println(it) }
