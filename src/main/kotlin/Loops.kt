279dbe93a6701706c7e4873dd61e3eca337d800b
oladejomubarak <oladejomubarakade@gmail.com>
2023-03-17 11:57:36 +0000

    loops

--- src/main/kotlin/Loops.kt
+++ src/main/kotlin/Loops.kt
@@ -2,4 +2,13 @@ fun main(){
     for (i in 1..10 ){
         println("i = ${i}")
     }
+    for (i in 1 until 10){
+        println(i)
+    }
+    for (i in 10 downTo 1){
+        println(i)
+    }
+    for (i in 1 until 10 step 2){
+        println(i)
+    }
 }
---
+++ src/main/kotlin/WhileLoop.kt
@@ -0,0 +1,6 @@
+fun main (){
+    var number = 0
+    while (number < 10){
+        println(++number)
+    }
+}
