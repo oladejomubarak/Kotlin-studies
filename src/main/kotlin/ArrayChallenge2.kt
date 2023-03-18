a7fb1b9f2c9a1004506bef8f7a8ff1c850b81f7b
oladejomubarak <oladejomubarakade@gmail.com>
2023-03-18 23:51:24 +0000

    array challenge

---
+++ src/main/kotlin/ArrayChallenge2.kt
@@ -0,0 +1,16 @@
+fun main (){
+    val nums : Array<Int> = arrayOf(3,4,23,3,78,657,8,90,75,45,67)
+    println(findMaxOrMinNum(nums, true))
+
+}
+fun findMaxOrMinNum(numbers: Array<Int>, searchForMax: Boolean):Int{
+    var minOrMax = numbers[0]
+    //var max = min
+    if (searchForMax){
+        for (i in numbers) if (i >  minOrMax) minOrMax = i
+        return minOrMax
+    } else {
+        for (i in numbers) if (i < minOrMax) minOrMax = i
+        return minOrMax
+    }
+}
