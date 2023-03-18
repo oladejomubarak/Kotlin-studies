15b0251003e8b67a1cb696e82e6e99540e0127a0
oladejomubarak <oladejomubarakade@gmail.com>
2023-03-18 22:28:50 +0000

    arrays

--- src/main/kotlin/Arrays.kt
+++ src/main/kotlin/Arrays.kt
@@ -1,3 +1,16 @@
 fun main(){
-    val alphanumerics = arrayOf("mub", "lap", "")
+    val alphanumerics = arrayOf("mub", "lap", "had", 1, 3, "lad", 7)
+    var counter = 0
+    for (i in alphanumerics){
+        if( i is Int){
+            counter++
+            println(i)
+        }
+
+    }
+    println("size is $counter")
+
+    for (j in alphanumerics){
+        if (j is String) println(j)
+    }
 }
