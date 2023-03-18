34759c875d94897cb4ec5f589564ef66e0cd4afa
oladejomubarak <oladejomubarakade@gmail.com>
2023-03-18 22:20:11 +0000

    arrays

--- src/main/kotlin/Challenge1.kt
+++ src/main/kotlin/Challenge1.kt
@@ -1,12 +1,14 @@
 fun main(){
     var sum : Int = 0
+    var counter = 0
     for (i in 1 .. 20){
         if (i % 2 == 0){
             sum += i
             println(i)
-
+            counter++
         }
     }
     println(sum)
+    println(counter)
 
 }
