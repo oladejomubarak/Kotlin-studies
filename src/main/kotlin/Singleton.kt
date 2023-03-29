6182e6cfdcf974bb65739e535b20f4db8678346c
oladejomubarak <oladejomubarakade@gmail.com>
2023-03-29 12:15:04 +0100

    singleton

--- src/main/kotlin/Singleton.kt
+++ src/main/kotlin/Singleton.kt
@@ -1,9 +1,24 @@
 fun main(){
+    println(Databases)
+    println(Databases)
+    println(Databases)
+    println(Databases)
 
 }
 class Database private constructor(){
     companion object{
+    private var instance : Database? = null
+        fun getInstance():Database?{
+            if (instance == null){
+                instance= Database()
+            }
+            return instance
+        }
 
-
+    }
+}
+object Databases{
+    init {
+        println("DB created")
     }
 }
