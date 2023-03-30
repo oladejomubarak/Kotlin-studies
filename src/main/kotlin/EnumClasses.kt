b31e5f9dffab17a0b73eabc9d6ee01a73772c19e
oladejomubarak <oladejomubarakade@gmail.com>
2023-03-30 13:51:51 +0100

    enum

--- src/main/kotlin/EnumClasses.kt
+++ src/main/kotlin/EnumClasses.kt
@@ -6,10 +6,16 @@ fun main(){
     println(DIRECTION.SOUTH.name)
     println(DIRECTION.WEST.direction)
 
+    println(DIRECTION.WEST.printData())
+
 }
 enum class DIRECTION(var direction: String, var distance: Int){
     NORTH("north", 30),
     SOUTH("south", 50),
     WEST("west", 70),
-    EAST("east", 80)
+    EAST("east", 80);
+
+    fun printData(){
+        println("direction: $direction, distance: $distance")
+    }
 }
